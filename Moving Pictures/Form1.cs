1fb10e5a085e8a5eee655c9c46da73f292568730
David Petticrew <David.Petticrew@riversidecollege.ac.uk>
2019-05-07 09:55:38 +0100

    Move in a square

--- Moving Pictures/Form1.cs
+++ Moving Pictures/Form1.cs
@@ -16,5 +16,17 @@ namespace Moving_Pictures
         {
             InitializeComponent();
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 50, pictureBox1.Location.Y);
+            await Task.Delay(500);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y+50);
+            await Task.Delay(500);
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y);
+            await Task.Delay(500);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y-50);
+            await Task.Delay(500);
+        }
     }
 }
